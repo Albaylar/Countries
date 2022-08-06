7a8f8a720ebd1823320bca70caa4aef7a537ad96
Albaylar <furkandenizalbaylar@gmail.com>
2022-08-06 08:38:33 +0300

    Initial Commit

---
+++ Countries/CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesApp.swift
+//  Countries
+//
+//  Created by Furkan Deniz Albaylar on 6.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
