c6c34a98b75a9b61240f074119d4070db2b75fb9
Albaylar <furkandenizalbaylar@gmail.com>
2022-08-08 17:35:31 +0300

    Updated

---
+++ Countries/Model/CountriesModel.swift
@@ -0,0 +1,34 @@
+//
+//  CountryItem.swift
+//  Countries
+//
+//  Created by Furkan Deniz Albaylar on 7.08.2022.
+//
+
+import Foundation
+import SwiftUI
+
+struct Countries: Codable {
+  let data: [CountryResult]
+}
+struct CountryResult: Codable, Identifiable, Hashable {
+  let code: String
+  let name: String
+  var id: String { code }
+}
+struct CountryDetail: Codable {
+  let data: CountryDetails
+  
+}
+struct CountryDetails: Codable {
+  let name: String
+  let code: String
+  let flagImageURI: String
+  let wikiDataID: String
+  
+  enum CodingKeys: String, CodingKey {
+    case wikiDataID = "wikiDataId"
+    case flagImageURI = "flagImageUri"
+    case name,code
+  }
+}
